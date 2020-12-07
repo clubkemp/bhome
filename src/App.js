216dae9eb1e0e14a8fca1db49fe85e576baa4cec
Jonny K <Kempj2.jk@gmail.com>
2020-12-06 16:35:10 -0800

    adding in a utils folder, which will hold our data json object. This should be robust enough for this data. If it grows or become more complicated, we might need a backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './utils/Data'
 
 function App() {
+  console.log(Data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App yo!</h1>
     </div>
   );
 }
