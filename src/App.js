9172350d4932a2acdcefa6c5936c91433b9ee6b6
Jonny K <Kempj2.jk@gmail.com>
2020-12-06 19:37:09 -0800

    material ui card template in place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {useState} from 'react'
+import Card from './components/Card'
 import Data from './utils/Data'
 
 function App() {
-  console.log(Data)
+  const [data, setData] = useState(Data)
+  console.log(data)
   return (
     <div>
+      <Card />
       <h1>App yo!</h1>
     </div>
   );
