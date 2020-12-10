9a76add1cc6d63c5ac2f03711fe3ff02ebd095f4
Jonny K <Kempj2.jk@gmail.com>
2020-12-09 17:32:02 -0800

    map initialized

--- src/components/MapComp/index.js
+++ src/components/MapComp/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import "./style.css";
 import Grid from '@material-ui/core/Grid';
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup
+  } from "react-leaflet";
 
 export default function MapComp() {
+    
+    const position = [51.505, -0.09]
     return (
         <Grid container item spacing={3} justify="center">
-            <h1>This is the map</h1>
+            <div id="mapWindow">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                </MapContainer>
+            </div>
         </Grid>
     )
 }
